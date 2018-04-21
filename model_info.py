27e217c55afb8598e84e7c950c98777eb59b4714
Adrian Lancucki <adrian.lancucki@cs.uni.wroc.pl>
2018-04-21 05:32:07 +0200

    Model_info prints accuracy

--- model_info.py
+++ model_info.py
@@ -4,13 +4,15 @@ import sys
 from pprint import pprint
 
 import tabulate
+import numpy as np
 
 import torch
+import tensorflow as tf
 
 import logger
 
 
-def model_details(model_dir):
+def model_details(model_dir, accuracy=False):
     state_file = os.path.join(model_dir, 'training_state.pkl')
     if not os.path.isfile(state_file):
         return None
@@ -24,12 +26,24 @@ def model_details(model_dir):
     details['file'] = args.get('file_class', 'UTF8File')
     details['args'] = args
     # print model_dir.split('/')[1], epoch, top_dir
+   
+    if accuracy:
+        steps = load_exp_steps(model_dir)
+        for s in ('train', 'valid'):
+            try:
+                val = steps[s][sorted(steps[s].keys())[-1]]['acc'][0]
+                val = np.round(val, 2)
+            except:
+                val = 'UNK'
+            details['acc_%s' % s] = val
     return details
 
-def print_summary(exp_all, include_args=False):
+def print_summary(exp_all, include_args=False, include_accuracy=False):
     exp_valid = [(name, d) for (name, d) in exp_all if d is not None]
     exp_empty = [(name, d) for (name, d) in exp_all if d is None]
     columns = ('name', 'data', 'model', 'file', 'epoch')
+    if include_accuracy:
+        columns += ('acc_valid', 'acc_train')
     if include_args:
         columns += ('args',)
     rows = [[d[col] for col in columns] for (name,d) in exp_valid]
@@ -38,6 +52,36 @@ def print_summary(exp_all, include_args=False):
         print 'Empty experiments:'
         for (name, _) in exp_empty:
             print name
+
+def parse_tf_steps(path):
+    steps = {}
+    for summary in tf.train.summary_iterator(path):
+        values = summary.summary.value
+        step = summary.step
+
+        assert len(values) <= 1, summary
+        if len(values) != 0:
+            if not steps.has_key(step):
+                steps[step] = {}
+            summ = steps[step]
+
+            tag = values[0].tag
+            sv = values[0].simple_value
+            if not summ.has_key(tag):
+                summ[tag] = []
+            summ[tag].append(sv)
+    return steps
+
+def load_split_steps(path, split):
+    ret = {}
+    path = os.path.join(path, split)
+    for f in [os.path.join(path, f) for f in os.listdir(path)]:
+        ret.update(parse_tf_steps(f))
+    return ret
+
+def load_exp_steps(path):
+    return {s: load_split_steps(path, s) \
+            for s in ('train', 'valid', 'test')}
 
 
 if __name__ == '__main__':
@@ -48,12 +92,14 @@ if __name__ == '__main__':
                         help='Print summary table')
     parser.add_argument('--args', action='store_true', default=False,
                         help='Print all args for an experiment')
+    parser.add_argument('--accuracy', action='store_true', default=False,
+                        help='Report accuracy')
     args = parser.parse_args()
 
     # dirs = [d for d in os.listdir('.') if d.startswith('2018-')]
-    exp_details = [(name, model_details(name)) for name in args.dirs]
+    exp_details = [(name, model_details(name, accuracy=args.accuracy)) for name in args.dirs]
     if args.table:
-        print_summary(exp_details, include_args=args.args)
+        print_summary(exp_details, include_args=args.args, include_accuracy=args.accuracy)
     else:
         for (name, d) in exp_details:
             print name
